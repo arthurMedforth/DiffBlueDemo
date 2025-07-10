f1af3a4dbf93a1b60bf97a281df35a4ee97d9790
arthurMedforth <arthur.medforth@accenture.com>
2025-07-10 09:04:41 +0100

    Updated javadoc

--- javadoc/type-search-index.js
+++ javadoc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.diffblue.demo","l":"BankAccountService"},{"p":"com.diffblue.demo","l":"BankAccountServiceTest"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.diffblue.demo","l":"BankAccountService"},{"p":"com.diffblue.demo","l":"BankAccountServiceTest"},{"p":"com.diffblue.demo","l":"BankAccountService.Transaction"},{"p":"com.diffblue.demo","l":"BankAccountService.TransactionType"}];updateSearchResults();
